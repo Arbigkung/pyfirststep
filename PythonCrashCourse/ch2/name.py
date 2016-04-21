b4f91d14615df6b96150ef5868bc7d67e290a073
arbigkung <contactpawat@gmail.com>
2016-04-21 10:33:02 +0700

    practice chap 2 with string

---
+++ PythonCrashCourse/ch2/name.py
@@ -0,0 +1,18 @@
+#Simple method
+name = "ada lovelace"
+print(name.title())
+
+print(name.upper())
+print(name.lower())
+
+#Combining or Concatenating String 
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+#Combining
+print(full_name)
+
+#Concatenating
+print("Hello, " + full_name.title() + "!") 
---
+++ PythonCrashCourse/ch2/tabandnewline.py
@@ -0,0 +1,7 @@
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#Strip white space
+favourite_language= ' python '
+print(favourite_language.rstrip())
+print(favourite_language.lstrip())
+print(favourite_language.strip())

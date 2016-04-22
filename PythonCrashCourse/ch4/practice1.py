ff27a1f835d2d3e5660b3aa621ec1ac5e52d4763
arbigkung <contactpawat@gmail.com>
2016-04-22 21:21:44 +0700

    practice loop

---
+++ PythonCrashCourse/ch4/practice1.py
@@ -0,0 +1,14 @@
+#Pizza
+
+pizzas = ['magarita', 'hawaii', 'super meat delurx']
+for pizza in pizzas:
+    print("I like " + pizza + " !")
+print('there is so yummy!')
+
+
+#Animal
+
+animals = ['dog', 'cat', 'rabbit']
+for animal in animals:
+    print ( animal + ' is human best friend')
+print('love them much')

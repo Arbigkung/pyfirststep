c953a1a1ea486d8f73c02411094c13602e8deff9
arbigkung <contactpawat@gmail.com>
2016-04-25 10:06:17 +0700

    practice kist of number

---
+++ PythonCrashCourse/ch4/number.py
@@ -0,0 +1,13 @@
+for value in range (1,5):
+    print(value)
+
+
+for value in range (1,6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+#Event numbers
+event_numbers = list(range(2,11,2))
+print(event_numbers) 

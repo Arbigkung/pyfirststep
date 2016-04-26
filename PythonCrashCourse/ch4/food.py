b781c3960e3aa3c8ed78160a19dda71635b0535c
arbigkung <contactpawat@gmail.com>
2016-04-26 10:33:46 +0700

    practice list slices and copy list

---
+++ PythonCrashCourse/ch4/food.py
@@ -0,0 +1,14 @@
+my_foods = ['pizza', 'falaful', 'carrot cake']
+#friend_foods = my_foods[:]
+
+#This doesn't work
+friend_foods = my_foods
+
+my_foods.append('connoli')
+friend_foods.append('ice cream')
+
+print("My favourite foods are:")
+print(my_foods)
+
+print("\nMy friend's favourite foods are:")
+print(friend_foods)

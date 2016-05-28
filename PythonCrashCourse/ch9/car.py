861f49afdd168306167b25b69b5139caa8829a85
arbigkung <contactpawat@gmail.com>
2016-05-28 20:21:27 +0700

    practice python import

--- PythonCrashCourse/ch9/car.py
+++ PythonCrashCourse/ch9/car.py
@@ -1,4 +1,4 @@
-class Car():
+class Car(object):
     """A simple attempt to represent a car."""
     
     def __init__(self, make, model, year):
@@ -31,15 +31,15 @@ class Car():
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
+#my_new_car = Car('audi', 'a4', 2016)
+#print(my_new_car.get_descriptive_name())
+#my_new_car.read_odometer()
 
 #my_new_car.odometer_reading = 23
 #my_new_car.read_odometer()
 
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
+#my_new_car.update_odometer(23)
+#my_new_car.read_odometer()
 
-my_new_car.increment_odometer(100)
-my_new_car.read_odometer()
+#my_new_car.increment_odometer(100)
+#my_new_car.read_odometer()

3ba41103a3ed4e3960ee7662dbeac61ecead56b8
Pawat <contactpawat@gmail.com>
2017-11-25 13:14:07 +0700

    add JS funciton part 1

---
+++ DjangoFullStackWebDev/JS_LEVEL_TWO/Part1.js
@@ -0,0 +1,11 @@
+// function hello(name = "Starnger") {
+//   console.log("hello "+name)
+// }
+//
+// function addNum(num1, num2){
+//   console.log(num1+num2)
+// }
+
+function formal(name="Sam", title="Sir"){
+  return title+ " " +name
+}

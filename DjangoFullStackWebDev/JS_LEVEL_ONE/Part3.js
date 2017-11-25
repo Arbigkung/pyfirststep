d77ca7da173df0f446b8927846742d4e867af139
Pawat <contactpawat@gmail.com>
2017-11-25 10:11:16 +0700

    add Part3.js

--- DjangoFullStackWebDev/JS_LEVEL_ONE/Part3.js
+++ DjangoFullStackWebDev/JS_LEVEL_ONE/Part3.js
@@ -1,4 +1,8 @@
-var lbs = prompt("What is the weight in pounds (lbs)?")
-var kg = lbs*0.454
-alert("That is:  "+kg+" kilograms")
-console.log("Conversion Completed")
+// var lbs = prompt("What is the weight in pounds (lbs)?")
+// var kg = lbs*0.454
+// alert("That is:  "+kg+" kilograms")
+// console.log("Conversion Completed")
+var lbs = prompt("Please enter pounds number")
+kilograms = lbs * 0.0454
+alert("Your kilogram is: " + kilograms)
+console.log("Convert is Completed")

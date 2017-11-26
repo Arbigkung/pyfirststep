6a4013cc010e47062cdc76f5c4f148cba2ccfc32
Pawat <contactpawat@gmail.com>
2017-11-26 20:21:14 +0700

    add jQuery game

---
+++ DjangoFullStackWebDev/jQuery/myscript.js
@@ -0,0 +1,16 @@
+// $('h1').click(function(){
+//   $(this).text("I was changed")
+// })
+
+
+// $('h1').on('dblclick', function() {
+//   $(this).toggleClass('turnBlue')
+// })
+
+// $('h1').on('mouseenter', function() {
+//   $(this).toggleClass('turnBlue')
+// })
+
+// $('input').eq(1).on('click',function(){
+//   $('.container').fadeOut(3000)
+// })
